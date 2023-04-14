b3a9d8115d679d9d98309d659e60d3a6d38c2a27
Ryan Hernandez <larh97@gmail.com>
2023-04-13 21:55:08 -0700

    second commit with argparse and update on non verbose option

--- main.py
+++ main.py
@@ -9,73 +9,78 @@ parser.add_argument("xytech", type=argparse.FileType('r'), help="files to proces
 parser.add_argument("--verbose", action="store_true", help="show verbose")
 
 args = parser.parse_args()
+dir = "Work"
 
+#flag for verbose
 if args.verbose:
-    for line in args.filename:
-        print(line.strip())
-    print("\n\n\n")
+    xytech_folders = []
     for line in args.xytech:
-        print(line.strip())
-else:
-    print("Sorry not verbose")
+        if "/" in line:
+            xytech_folders.append(line)
 
+    for line in args.filename:
+        line_parse = line.split(" ")
+        current_folder = line_parse.pop(0)
+        sub_folder = current_folder.replace("/images1/starwars", "")
+        new_location = ""
 
-"""
-#Open Xytech file
-xytech_file_location = "Xytech.txt"
-xytech_folders = []
+        # Folder replace check
+        for xytech_line in xytech_folders:
+            if sub_folder in xytech_line:
+                new_location = xytech_line.strip()
 
-read_xytech_file = open(xytech_file_location, "r")
-for line in read_xytech_file:
-    if "/" in line:
-        xytech_folders.append(line)
-
-#Open Baselight file
-baselight_file_location = "Baselight_export.txt"
-read_baselight_file = open(baselight_file_location, "r")
-
-#Read each line from Baselight file
-for line in read_baselight_file:
-    line_parse = line.split(" ")
-    current_folder = line_parse.pop(0)
-    sub_folder = current_folder.replace("/images1/starwars", "")
-    new_location = ""
-    #Folder replace check
-    for xytech_line in xytech_folders:
-        if sub_folder in xytech_line:
-            new_location = xytech_line.strip()
-    first=""
-    pointer=""
-    last=""
-    for numeral in line_parse:
-        #Skip <err> and <null>
-        if not numeral.strip().isnumeric():
-            continue
-        #Assign first number
-        if first == "":
-            first = int(numeral)
-            pointer = first
-            continue
-        #Keeping to range if succession
-        if int(numeral) == (pointer+1):
-            pointer = int(numeral)
-            continue
-        else:
-            #Range ends or no sucession, output
-            last = pointer
-            if first == last:
-                print ("%s %s" % (new_location, first))
+        first = ""
+        last = ""
+        pointer = ""
+        for numeral in line_parse:
+            # Skip <err> and <null>
+            if not numeral.strip().isnumeric():
+                continue
+            # Assign first number
+            if first == "":
+                first = int(numeral)
+                pointer = first
+                continue
+            # Keeping to range if succession
+            if int(numeral) == (pointer + 1):
+                pointer = int(numeral)
+                continue
             else:
-                print ("%s %s-%s" % (new_location, first, last))
-            first= int(numeral)
-            pointer=first
-            last=""
-    #Working with last number each line
-    last = pointer
-    if first != "":
-        if first == last:
-            print ("%s %s" % (new_location, first))
-        else:
-            print ("%s %s-%s" % (new_location, first, last))
+                # Range ends or no sucession, output
+                last = pointer
+                if first == last:
+                    print("%s %s" % (new_location, first))
+                else:
+                    print("%s %s-%s" % (new_location, first, last))
+                first = int(numeral)
+                pointer = first
+                last = ""
+            # Working with last number each line
+        last = pointer
+        if first != "":
+            if first == last:
+                print("%s %s" % (new_location, first))
+            else:
+                print("%s %s-%s" % (new_location, first, last))
 
-"""
+
+#if verbose is not flagged
+else:
+    xytech_folders = []
+    for line in args.xytech:
+        if "/" in line:
+            xytech_folders.append(line)
+    print(xytech_folders)
+
+    new_location =[]
+    for line in args.filename:
+        line_parse = line.split(" ")
+        current_folder = line_parse.pop(0)
+        sub_folder = current_folder.replace("/images1/starwars", "")
+        new_location = ""
+        # Folder replace check
+        for xytech_line in xytech_folders:
+            if sub_folder in xytech_line:
+                new_location = xytech_line.strip()
+
+    print("not verbose")
