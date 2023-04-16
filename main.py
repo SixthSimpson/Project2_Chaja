697ad9072654ada14b013b2908b16a457dcd0a11
SixthSimpson <larh97@gmail.com>
2023-04-16 02:09:50 -0700

    second to last commit hopefully lol added console and csv as well as database calls and cleaned code

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 #Place above so you don't have to call python from commandline
 import csv
-import time
+import datetime
 import os
 import argparse
 import pymongo
@@ -13,80 +13,35 @@ mycol2 = mydb["container 2"]
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--filename", type=argparse.FileType('r'), help="files to process")
-parser.add_argument("--xytech", type=argparse.FileType('r'), help="files to process")
-parser.add_argument("--verbose", action="store_true", help="show verbose")
+parser.add_argument("-f", "--files", nargs='+', dest='filename', type=argparse.FileType('r'), help="files to process")
+parser.add_argument("-x", "--xytech", type=argparse.FileType('r'), help="files to process", required=True)
+parser.add_argument("-v", "--verbose", action="store_true", help="show console output")
+parser.add_argument("-o", "--output", action="store_true", help="output to csv/ default to database")
 
 args = parser.parse_args()
-dir = "Work"
-
-#flag for verbose
-if args.verbose:
-    xytech_folders = []
-    for line in args.xytech:
-        if "/" in line:
-            xytech_folders.append(line)
-
-    for line in args.filename:
-        line_parse = line.split(" ")
-        current_folder = line_parse.pop(0)
-        sub_folder = current_folder.replace("/images1/", "")
-        new_location = ""
-
-        # Folder replace check
-        for xytech_line in xytech_folders:
-            if sub_folder in xytech_line:
-                new_location = xytech_line.strip()
-
-        first = ""
-        last = ""
-        pointer = ""
-        for numeral in line_parse:
-            # Skip <err> and <null>
-            if not numeral.strip().isnumeric():
-                continue
-            # Assign first number
-            if first == "":
-                first = int(numeral)
-                pointer = first
-                continue
-            # Keeping to range if succession
-            if int(numeral) == (pointer + 1):
-                pointer = int(numeral)
-                continue
-            else:
-                # Range ends or no sucession, output
-                last = pointer
-                if first == last:
-                    print("%s %s" % (new_location, first))
-                else:
-                    print("%s %s-%s" % (new_location, first, last))
-                first = int(numeral)
-                pointer = first
-                last = ""
-            # Working with last number each line
-        last = pointer
-        if first != "":
-            if first == last:
-                print("%s %s" % (new_location, first))
-            else:
-                print("%s %s-%s" % (new_location, first, last))
+print(args.filename)
+# xytech folder handling
+xytech_folders = []
+for line in args.xytech:
+    if "/" in line:
+        xytech_folders.append(line)
 
 
-#if verbose is not flagged
-else:
-    #xytech folder handling
-    xytech_folders = []
-    for line in args.xytech:
-        if "/" in line:
-            xytech_folders.append(line)
-
+# go through all files in the input values
+for files in args.filename:
     frames = []
-    for line in args.filename:
+    db_parse = files.name.split("_")
+    machine = db_parse.pop(0)
+    for line in files:
         line_parse = line.split(" ")
-        current_folder = line_parse.pop(0)
-        sub_folder = current_folder.replace("/images1/", "")
-        new_location = ""
+        if machine == "Flame":
+            current_folder = line_parse.pop(1)
+            sub_folder = current_folder
+            new_location = ""
+        else:
+            current_folder = line_parse.pop(0)
+            sub_folder = current_folder.replace("/images1/", "")
+            new_location = ""
 
         # Folder replace check
         for xytech_line in xytech_folders:
@@ -110,7 +65,7 @@ else:
                 pointer = int(numeral)
                 continue
             else:
-                # Range ends or no sucession, output
+                # Range ends or no succession, output
                 last = pointer
                 if first == last:
                     str = ("%s %s" % (new_location, first))
@@ -130,42 +85,51 @@ else:
             else:
                 str = ("%s %s-%s" % (new_location, first, last))
                 frames.append(str)
-
     ##tear apart the file name and seperate the machine, user, and the date split the extension into a tuple
-    db_parse = args.filename.name.split("_")
-    machine = db_parse.pop(0)
     user = db_parse.pop(0)
     d = db_parse.pop(0)
     d1 = os.path.splitext(d)
     date = d1[0]
-    sub_date = time.localtime()
+    sub_date = datetime.datetime.now()
 
-    #test that data is being stored correctly
-    print(machine)
-    print(user)
-    print(date)
-    print(time.asctime(sub_date))
-    uid = os.uname().nodename
-    print(uid)
-
-    #put in a col and insert to DB
-    mydict1 = {"machine": machine, "user": user, "date": date, "submitted date": sub_date}
-    #x = mycol.insert_one(mydict1)
+    # put in a col and insert to DB
+    # mydict1 = {"script runner: ": uid, "machine": machine, "user": user, "date": date, "submitted date": sub_date}
+    # x = mycol.insert_one(mydict1)
 
     mydict2 = {"user": user, "date": date, "frames to fix": frames}
-    #x = mycol2.insert_one(mydict2)
+    x = mycol2.insert_one(mydict2)
 
-    # csv file beggining
-    csv_file = 'output.csv'
-    with open(csv_file, 'w', newline='') as f:
-        writer = csv.writer(f)
-        writer.writerow(['Machine', 'User on File', 'Date of File', 'Submitted Date'])
-        writer.writerow([machine, user, date, sub_date])
-        writer.writerow(['Location and Frames to Fix'])
+    if args.output:
+        # csv file beggining
+        csv_file = 'output.csv'
+        with open(csv_file, 'w', newline='') as f:
+            writer = csv.writer(f)
+            writer.writerow(['Machine', 'User on File', 'Date of File', 'Submitted Date'])
+            writer.writerow([machine, user, date, sub_date])
+            writer.writerow(['Location and Frames to Fix'])
 
-        for frame in frames:
-            writer.writerow(frame)
+            for frame in frames:
+                writer.writerow(frame)
 
+if args.verbose:
+    for files in args.filename:
+        db_parse = files.name.split("_")
+        machine = db_parse.pop(0)
+        user = db_parse.pop(0)
+        d = db_parse.pop(0)
+        d1 = os.path.splitext(d)
+        date = d1[0]
+        sub_date = datetime.datetime.now()
+        # test that data is being stored correctly
+        print(machine)
+        print(user)
+        print(date)
+        print(sub_date.strftime("%c"))
+        # uid = os.uname().nodename
+        # print(uid)
     for frame in frames:
         print(frame)
-    print("not verbose")
+
+
+
+
